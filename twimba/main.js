84de2ed5a9db5bc467e4f837a176bcbc47b0f22b
andrea.nvr <an.nvr@gmail.com>
2023-09-01 06:32:03 +1000

    Update main.js

--- twimba/main.js
+++ twimba/main.js
@@ -1,1 +1,52 @@
 import { tweetsData } from "./data.js";
+
+const tweetInput = document.getElementById("tweet-input")
+const tweetBtn = document.getElementById("tweet-btn")
+
+tweetBtn.addEventListener("click", () => {
+  console.log(tweetInput.value)
+})
+
+document.addEventListener("click", (e) => {
+  console.log(e.target.dataset.like)
+})
+
+function getFeedHtml(){
+  let feedHtml = ""
+  tweetsData.forEach((tweet) => {
+    feedHtml += `
+      <div class="tweet">
+      <div class="tweet-inner">
+          <img src="${tweet.profilePic}" class="profile-pic">
+          <div>
+              <p class="handle">${tweet.handle}</p>
+              <p class="tweet-text">${tweet.tweetText}</p>
+              <div class="tweet-details">
+                  <span class="tweet-detail">
+                    <i class="fa-regular fa-comment-dots" data-reply="${tweet.uuid}"></i>
+                      ${tweet.replies.length}
+                  </span>
+                  <span class="tweet-detail">
+                    <i class="fa-solid fa-heart" data-like=${tweet.uuid}></i>
+                      ${tweet.likes}
+                  </span>
+                  <span class="tweet-detail">
+                    <i class="fa-solid fa-retweet" data-retweet=${tweet.uuid}></i>
+                      ${tweet.retweets}
+                  </span>
+              </div>   
+          </div>            
+      </div>
+  </div>
+  `
+  })
+  return feedHtml
+}
+
+
+function render(){
+  const feedEl = document.getElementById("feed")
+  feedEl.innerHTML = getFeedHtml()
+}
+
+render()
